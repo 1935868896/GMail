ceb62f30b433378f26ce1b72edb2d41659f2f9de
1935868896 <1935868896@qq.com>
2020-09-16 17:17:21 +0800

    谷粒商城-分布式高级篇---认证服务功能 部分

---
+++ gmail-auth-server/src/test/java/com/zc/gmail/auth/GmailAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.gmail.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
